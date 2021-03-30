cec92f376e161f66821b6209542e798991ef5c3c
SergeLavrentev <bert679@yandex.ru>
2021-03-30 12:10:46 +0400

    add mongo_db

--- app.js
+++ app.js
@@ -1,14 +1,24 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const mongoose = require("mongoose");
 const path = require("path");
 
 const indexRouter = require("./routes/index.js");
 const userRouter = require("./routes/users");
 
+// DB connection
+
+const MONGO_DB_URI = process.env.MONGO_DB_URI;
+
+mongoose
+  .connect(MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("Mongo db connected"))
+  .catch((err) => console.log(err));
+
 // template engine
 app.set("views", path.resolve(__dirname, "views"));
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 // routes
 app.use("/", indexRouter);
