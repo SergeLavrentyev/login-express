63184748b237301c6b57090893aa3658272092f6
SergeLavrentev <bert679@yandex.ru>
2021-03-30 12:21:30 +0400

    add user_model

---
+++ models/User.js
@@ -0,0 +1,24 @@
+const {Schema, model} = require("mongoose");
+
+const UserSchema = new Schema({
+  name: {
+    type: String,
+    require: true,
+  },
+  email: {
+    type: String,
+    require: true,
+  },
+  password: {
+    type: String,
+    require: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+const User = model("User", UserSchema);
+
+module.exports = User;
