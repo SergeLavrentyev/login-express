ebb66d770b2e51da9628b12259702bf3a7da59e0
SergeLavrentev <bert679@yandex.ru>
2021-03-30 19:23:16 +0400

    add config

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
-require("dotenv").config();
 const express = require("express");
 const app = express();
-const mongoose = require("mongoose");
 const session = require("express-session");
 const flash = require("connect-flash");
 const passport = require("passport");
 const path = require("path");
 const auth = require("./auth/pasport");
+const { connect } = require("mongoose");
+const { DB, PORT, SECRET } = require("./config");
 
 const indexRoute = require("./routes/index.js");
 const userRoute = require("./routes/users");
@@ -18,10 +18,7 @@ auth(passport);
 
 // DB connection
 
-const MONGO_DB_URI = process.env.MONGO_DB_URI;
-
-mongoose
-  .connect(MONGO_DB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+connect(DB, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log("Mongo db connected"))
   .catch((err) => console.log(err));
 
@@ -34,9 +31,9 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "secret",
-    resave: true,
-    saveUninitialized: true,
+    secret: SECRET,
+    resave: false,
+    saveUninitialized: false,
   })
 );
 
@@ -50,7 +47,5 @@ app.use("/users", userRoute);
 app.use("/useradmin", userAdminRoute);
 
 // start app
-
-const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => console.log(`Serve at http://localhost:${PORT}`));
---
+++ config/index.js
@@ -0,0 +1,7 @@
+require("dotenv").config();
+
+module.exports = {
+  DB: process.env.MONGO_DB_URI,
+  PORT: process.env.PORT,
+  SECRET: process.env.SECRET
+}
