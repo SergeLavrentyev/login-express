cfcb2d39daf2101383a82ea2b8015b23292dafda
SergeLavrentev <bert679@yandex.ru>
2021-03-30 10:49:04 +0400

    add user routes

---
+++ routes/users.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router();
+
+// Login Page
+
+router.get("/login", (req, res) => {
+  res.send("login");
+});
+
+//  Register Page
+
+router.get("/register", (req, res) => {
+  res.send("register");
+});
+
+module.exports = router;
